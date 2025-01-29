2c3b527f641c5b8cc6465def2c95e76282976ef1
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2025-01-28 23:32:43 -0300

    Updated projeto contador

--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -18,28 +19,73 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void decrement() {
+    print("Decrement");
+  }
+
+  void increment() {
+    print("Increment");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text("Pode entrar", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-          Text(
-            "11",
-            style: TextStyle(fontSize: 48),
+          Text("Pode entrar!",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              )),
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: Text(
+              "11",
+              style: TextStyle(fontSize: 96, color: Colors.white),
+            ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Saiu"),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: Size(96, 96),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16),
+                    ),
+                  ),
+                ),
+                onPressed: decrement,
+                child: Text(
+                  "Saiu",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
               ),
-              SizedBox(width: 16),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Entrou"),
+              SizedBox(width: 24),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: Size(96, 96),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16),
+                    ),
+                  ),
+                ),
+                onPressed: increment,
+                child: Text("Entrou",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    )),
               )
             ],
           )
